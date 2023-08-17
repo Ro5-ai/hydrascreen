bb674c0821b8ca18def3485c840a7095dc82f5ce
Orestis <obastas@ro5.ai>
2023-08-17 13:05:37 +0300

    Bump version: 0.0.2 → 0.0.3

--- hydrascreen/__init__.py
+++ hydrascreen/__init__.py
@@ -1,7 +1,7 @@
 from hydrascreen.api import APICredentials
 from hydrascreen.predictor import HydraScreen
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 
 def login(email: str, organization: str) -> HydraScreen:
